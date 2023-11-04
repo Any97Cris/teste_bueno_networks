08e4015d8731088ebfa5041764c5003a5eef0e9c
Cris97Pidge <criscianysilva1997@gmail.com>
2023-11-04 19:12:11 -0400

    tela common

---
+++ resources/views/common.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.head')
+<div class="container">
+    <h2 class="mb-5">Usuários Admin</h2>
+        <div class="row justify-content-md-center cotainer">
+            <div class="col-4">
+                
+            </div>
+        </div>
+    </div>
+@extends('layouts.footer')
