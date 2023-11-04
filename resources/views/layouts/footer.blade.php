5b7704cbfd615016bbf1e99be61e8f4cb3b2f55a
Cris97Pidge <criscianysilva1997@gmail.com>
2023-11-04 19:42:01 -0400

    configuraçao datatable

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,4 +2,10 @@
     <footer>
         Desenvolvidor por <a href="https://github.com/Any97Cris" target="_blank">Crisciany Silva</a>
     </footer>
-</html>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script>
+        let table = new DataTable('#myTable');
+    </script>
+    </html>
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -14,6 +14,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="css/app.css">
         
+        <link rel="stylesheet" href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     
     
     </head>
