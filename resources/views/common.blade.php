921d72b902cdcbb7e7e6535e3758ae0ee69a23d3
Cris97Pidge <criscianysilva1997@gmail.com>
2023-11-04 19:42:30 -0400

    configuracao listar usuário

--- resources/views/common.blade.php
+++ resources/views/common.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.head')
 <div class="container">
-    <h2 class="mb-5">Usuários Admin</h2>
+    <h2 class="mb-5">Usuários Common</h2>
         <div class="row justify-content-md-center cotainer">
-            <div class="col-4">
-                
+            <div class="col-12">
+                <table id="myTable" class="display">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Teste 1</td>
+                            <td>Teste 2</td>
+                        </tr>
+                        <tr>
+                            <td>teste1@gmail.com</td>
+                            <td>teste2@gmail.com</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
