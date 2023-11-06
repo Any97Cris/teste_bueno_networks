c0d3163b1234811d8b49b6ce86e5b0ef9a899335
Cris97Pidge <criscianysilva1997@gmail.com>
2023-11-06 10:11:48 -0400

    criacao tela editar

---
+++ resources/views/editar.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.head')
+<div class="container">
+<h2 class="mb-5">Editar</h2>
+    <div class="row justify-content-md-center cotainer">
+        <div class="col-4">
+            <form>
+                <!-- Name input -->
+                <div class="form-outline mb-4">
+                    <input type="text" id="nome" name="name" class="form-control" />
+                    <label class="form-label" for="form2Example1">Nome e Sobrenome</label>
+                </div>
+
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                    <input type="email" id="email" name="email" class="form-control" />
+                    <label class="form-label" for="form2Example1">Email</label>
+                </div>
+
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <input type="password" id="password" name="password" class="form-control" />
+                    <label class="form-label" for="password">Password</label>
+                </div>
+                
+
+                <!-- Submit button -->
+                <div class="d-grid gap-2">
+                    <button type="button" class="btn btn-primary btn-block mb-4">Salvar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@extends('layouts.footer')
