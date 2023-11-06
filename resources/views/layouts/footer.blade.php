8e134512a2b3105fbadeff6af8aa8001f688557d
Cris97Pidge <criscianysilva1997@gmail.com>
2023-11-06 15:11:09 -0400

    ajuste footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
     </body>
     <footer>
-        Desenvolvidor por <a href="https://github.com/Any97Cris" target="_blank">Crisciany Silva</a>
+        Desenvolvidor por <a href="https://github.com/Any97Cris" class="text-center" target="_blank">Crisciany Silva</a>
     </footer>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
