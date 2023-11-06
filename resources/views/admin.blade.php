b337c7a736af1f8475e6fb3d6b4deb79c2513bb3
Cris97Pidge <criscianysilva1997@gmail.com>
2023-11-06 10:10:47 -0400

    ajuste redirecionamento de tela nos btn add e edit

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.head')
 <div class="container">
     <h2 class="mb-4">Usuários Admin</h2>
-    <button type="button" class="btn btn-primary mb-5 btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <a href="{{ route('tela-cadastrar') }}" class="btn btn-primary mb-5 btn-sm">
         Cadastrar
-    </button>
+    </a>
     <div class="row justify-content-md-center cotainer">
         <div class="col-12">
             <table id="myTable" class="display">
@@ -25,7 +25,7 @@
                                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
                                 </svg>
                             </a>
-                            <a href="#" class="text-dark" data-bs-toggle="modal" data-bs-target="#edit_button">
+                            <a href="{{route('tela-editar')}}" class="text-dark">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -38,69 +38,5 @@
         </div>
     </div>
 </div>
-
-<!-- Modal Cadastrar-->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Cadastrar</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <div class="mb-3">
-                <label for="for_name" class="form-label">Nome e Sobrenome</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Nome e Sobrenome">
-              </div>
-            <div class="mb-3">
-            <label for="for_email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
-            </div>
-            <div class="mb-3">
-                <label for="for_password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="*****password*****">
-              </div>
-              
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
-          <button type="button" class="btn btn-primary">Cadastrar</button>
-        </div>
-      </div>
-    </div>
-  </div>
-<!-- Fim Modal Cadastrar -->
-
-<!-- Modal Editar-->
-<div class="modal fade" id="edit_button" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Editar</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <div class="mb-3">
-                <label for="for_name" class="form-label">Nome e Sobrenome</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Nome e Sobrenome">
-              </div>
-            <div class="mb-3">
-            <label for="for_email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
-            </div>
-            <div class="mb-3">
-                <label for="for_password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="*****password*****">
-              </div>
-              
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
-          <button type="button" class="btn btn-primary">Cadastrar</button>
-        </div>
-      </div>
-    </div>
-  </div>
-<!-- Fim Modal Editar -->
 
 @extends('layouts.footer')
